cba3a31ce23e1ac150a42bc535afbf418f3e2304
Mike Cecconello <mikececco2000@gmail.com>
2024-06-12 20:19:27 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -7,65 +7,73 @@ import { logger } from '#root/logger.js'
 import { createServer, createServerManager } from '#root/server/index.js'
 import { prisma } from '#root/prisma/index.js'
 
-try {
+function onShutdown(cleanUp: () => Promise<void>) {
+  let isShuttingDown = false
+  const handleShutdown = async () => {
+    if (isShuttingDown)
+      return
+    isShuttingDown = true
+    logger.info('Shutdown')
+    await cleanUp()
+  }
+  process.on('SIGINT', handleShutdown)
+  process.on('SIGTERM', handleShutdown)
+}
+
+async function startPolling() {
   const bot = createBot(config.BOT_TOKEN, {
     prisma,
   })
-  const server = await createServer(bot)
-
   // graceful shutdown
   onShutdown(async () => {
-    logger.info('Shutdown')
-
     await bot.stop()
   })
 
-  await prisma.$connect()
+  // start bot
+  await bot.start({
+    allowed_updates: config.BOT_ALLOWED_UPDATES,
+    onStart: ({ username }) =>
+      logger.info({
+        msg: 'Bot running...',
+        username,
+      }),
+  })
+}
 
-  if (config.BOT_MODE === 'webhook') {
-    // to prevent receiving updates before the bot is ready
-    await bot.init()
+async function startWebhook() {
+  const bot = createBot(config.BOT_TOKEN, {
+    prisma,
+  })
+  const server = createServer(bot)
+  const serverManager = createServerManager(server)
 
-    // start server
-    serve(
-      {
-        fetch: server.fetch,
-        hostname: config.BOT_SERVER_HOST,
-        port: config.BOT_SERVER_PORT,
-      },
-      (info) => {
-        const url
-        = info.family === 'IPv6'
-          ? `http://[${info.address}]:${info.port}`
-          : `http://${info.address}:${info.port}`
+  // graceful shutdown
+  onShutdown(async () => {
+    await serverManager.stop()
+  })
 
-        logger.info({
-          msg: 'Server started',
-          url,
-        })
-      },
-    )
+  // to prevent receiving updates before the bot is ready
+  await bot.init()
 
-    // set webhook
-    // await bot.api.setWebhook(config.BOT_WEBHOOK, {
-    //   allowed_updates: config.BOT_ALLOWED_UPDATES,
-    //   secret_token: config.BOT_WEBHOOK_SECRET,
-    // })
-    // logger.info({
-    //   msg: 'Webhook was set',
-    //   url: config.BOT_WEBHOOK,
-    // })
-  }
-  else if (config.BOT_MODE === 'polling') {
-    await bot.start({
-      allowed_updates: config.BOT_ALLOWED_UPDATES,
-      onStart: ({ username }) =>
-        logger.info({
-          msg: 'Bot running...',
-          username,
-        }),
-    })
-  }
+  // start server
+  const info = await serverManager.start(
+    config.BOT_SERVER_HOST,
+    config.BOT_SERVER_PORT,
+  )
+  logger.info({
+    msg: 'Server started',
+    url:
+      info.family === 'IPv6'
+        ? `http://[${info.address}]:${info.port}`
+        : `http://${info.address}:${info.port}`,
+  })
+}
+
+try {
+  if (config.BOT_MODE === 'webhook')
+    await startWebhook()
+  else if (config.BOT_MODE === 'polling')
+    await startPolling()
 }
 catch (error) {
   logger.error(error)
